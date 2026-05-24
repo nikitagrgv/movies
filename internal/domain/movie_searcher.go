592eb0df243f69f412861b182e66538175b35b81
Nikita Grigorev <pettyaelse@gmail.com>
2026-05-24 17:53:49 +0700

    w

--- internal/domain/movie_searcher.go
+++ internal/domain/movie_searcher.go
@@ -1,1 +1,13 @@
 package domain
+
+import "context"
+
+type SearchMovieResult struct {
+	Movies      []Movie
+	CurrentPage int
+	TotalPages  int
+}
+
+type MovieSearcher interface {
+	SearchMovie(ctx context.Context, query string, page int) (SearchMovieResult, error)
+}
--- internal/usecase/search_movies.go
+++ internal/usecase/search_movies.go
@@ -1,1 +1,24 @@
 package usecase
+
+import (
+	"context"
+
+	"github.com/nikitagrgv/movies/internal/domain"
+)
+
+type SearchMoviesParams struct {
+	Query string
+	Page  int
+}
+
+type SearchMovieUsecase struct {
+	searcher domain.MovieSearcher
+}
+
+func NewSearchMovieUsecase(searcher domain.MovieSearcher) *SearchMovieUsecase {
+	return &SearchMovieUsecase{searcher: searcher}
+}
+
+func (u *SearchMovieUsecase) SearchMovie(ctx context.Context, query string, page int) (domain.SearchMovieResult, error) {
+	return u.searcher.SearchMovie(ctx, query, page)
+}
