7d3f621536a3fca38722eff0fe20bd64ed50d5d7
Nikita Grigorev <pettyaelse@gmail.com>
2026-06-05 22:55:08 +0700

    w

--- internal/server/middleware.go
+++ internal/server/middleware.go
@@ -1,9 +1,7 @@
 package server
 
 import (
-	"log"
 	"net/http"
-	"runtime/debug"
 )
 
 type Middleware func(http.Handler) http.Handler
@@ -13,25 +11,4 @@ func Chain(handler http.Handler, middlewares ...Middleware) http.Handler {
 		handler = middlewares[i](handler)
 	}
 	return handler
-}
-
-func StripPrefixMiddleware(prefix string) Middleware {
-	return func(next http.Handler) http.Handler {
-		return http.StripPrefix(prefix, next)
-	}
-}
-
-func RecoveryMiddleware() Middleware {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			defer func() {
-				if err := recover(); err != nil {
-					log.Printf("[PANIC RECOVERED] %v\nStack Trace:\n%s", err, debug.Stack())
-					w.Header().Set("Connection", "close")
-					http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-				}
-			}()
-			next.ServeHTTP(w, r)
-		})
-	}
 }
