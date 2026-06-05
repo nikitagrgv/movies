f2ce0ce5b70f414288a94e3296a5cd27ca15829a
Nikita Grigorev <pettyaelse@gmail.com>
2026-06-05 22:52:57 +0700

    w

--- internal/server/middleware.go
+++ internal/server/middleware.go
@@ -1,6 +1,10 @@
 package server
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"runtime/debug"
+)
 
 type Middleware func(http.Handler) http.Handler
 
@@ -11,8 +15,23 @@ func Chain(handler http.Handler, middlewares ...Middleware) http.Handler {
 	return handler
 }
 
-func StripPrefix(prefix string) Middleware {
+func StripPrefixMiddleware(prefix string) Middleware {
 	return func(next http.Handler) http.Handler {
 		return http.StripPrefix(prefix, next)
+	}
+}
+
+func RecoveryMiddleware() Middleware {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			defer func() {
+				if err := recover(); err != nil {
+					log.Printf("[PANIC RECOVERED] %v\nStack Trace:\n%s", err, debug.Stack())
+					w.Header().Set("Connection", "close")
+					http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+				}
+			}()
+			next.ServeHTTP(w, r)
+		})
 	}
 }
--- internal/web/routes.go
+++ internal/web/routes.go
@@ -18,20 +18,24 @@ func (h *Handler) RegisterRoutes(mux *http.ServeMux) {
 
 	mux.Handle("/static/", server.Chain(
 		staticHandler,
-		server.StripPrefix("/static/"),
+		server.RecoveryMiddleware(),
+		server.StripPrefixMiddleware("/static/"),
 		server.GzipMiddleware,
 	))
 	mux.Handle("/favicon.ico", server.Chain(
 		staticHandler,
+		server.RecoveryMiddleware(),
 		server.GzipMiddleware,
 	))
 
 	mux.Handle("GET /{$}", server.Chain(
 		http.HandlerFunc(h.ShowMain),
+		server.RecoveryMiddleware(),
 	))
 
 	mux.Handle("GET /search", server.Chain(
 		http.HandlerFunc(h.HandleSearch),
+		server.RecoveryMiddleware(),
 	))
 
 	mux.Handle("GET /movie/{id}", server.Chain(
@@ -39,6 +43,7 @@ func (h *Handler) RegisterRoutes(mux *http.ServeMux) {
 			id := r.PathValue("id")
 			h.HandleMovie(id, w, r)
 		}),
+		server.RecoveryMiddleware(),
 	))
 
 	mux.Handle("GET /tv/{id}", server.Chain(
@@ -46,9 +51,11 @@ func (h *Handler) RegisterRoutes(mux *http.ServeMux) {
 			id := r.PathValue("id")
 			h.HandleTvShow(id, w, r)
 		}),
+		server.RecoveryMiddleware(),
 	))
 
 	mux.Handle("/", server.Chain(
 		http.HandlerFunc(h.ShowNotFound),
+		server.RecoveryMiddleware(),
 	))
 }
