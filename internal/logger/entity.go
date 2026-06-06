f5217d299121318c73bbd0ab9c215c4ea3cb3c60
Nikita Grigorev <pettyaelse@gmail.com>
2026-06-07 00:24:35 +0700

    w

---
+++ internal/logger/entity.go
@@ -0,0 +1,5 @@
+package logger
+
+type Visit struct {
+	Path string
+}
--- migrations/000001_create_logs.up.sql
+++ migrations/000001_create_logs.up.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS logs.visits
     id           INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     ip_address   INET NOT NULL,
     attempted_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    url          TEXT NOT NULL
+    path         TEXT NOT NULL
 );
 
 CREATE INDEX IF NOT EXISTS idx_visits_attempted_at ON logs.visits (attempted_at DESC);
